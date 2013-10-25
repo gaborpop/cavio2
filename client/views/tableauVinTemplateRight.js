3efb4cc962e0ca805ec81e3570c73df7101e37c2
gaborpop <popgabor@gmail.com>
2013-10-25 14:04:08 +0000

    Fini macave

--- client/views/tableauVinTemplateRight.js
+++ client/views/tableauVinTemplateRight.js
@@ -1,6 +1,14 @@
 Template.tableauVinTemplateRight.helpers({
   vinsBus: function() {
     return Vins.find({statusVin : 'Bu'}, {sort: {submitted: -1}});
+  },
+  submittedModifiedText: function() {
+    var newDate = new Date(this.modifiedStatus);
+    var month = newDate.getMonth();
+    var day = newDate.getDate();
+    var year = newDate.getFullYear()
+    
+    return (day + "." + month + "." + year); 
   }
 
   
