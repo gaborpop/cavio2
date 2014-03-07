cd9bd2c1d83a770831813b0355665278b6d84867
gaborpop <popgabor@gmail.com>
2014-03-07 15:45:11 +0000

    nouveau tableau vins

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ if(Meteor.isClient){
 }
 
 Hooks.onLoggedIn = function () {
-    Router.go('maCaveTemplateListe');
+    
 }
 
 Hooks.onLoggedOut = function () {
--- client/views/tableauVinTemplate.js
+++ client/views/tableauVinTemplate.js
@@ -29,15 +29,46 @@ Template.tableauVinTemplate.helpers({
       return (true); 
     
     
-  }
+  },
+	settings: function () {
+		i18n.setLanguage('fr');
+        return { fields: [
+    { key: 'nomVin', label: 'Producteur' },
+    { key: 'regionVin', label: 'Région' },
+		{ key: 'couleurVin', label: 'Couleur' },			
+    { key: 'appellationVin', label: 'Appellation' },
+		{ key: 'anneeVin', label: 'Année' },
+		{ key: 'noteVin', label: 'Note' },
+		{ key: 'cavemin',
+        label: 'Garde',
+		 fn: function (value, object) { return object.cavemin + "/" + object.cavemax; }
+    },
+		{ key: 'tempmin', 
+					label: 'T°',
+		 fn: function (value, object) { return object.tempmin + "/" + object.tempmax; }
+    },
+		{ key: 'commentaireVin', label: 'Avis' },			
+		{ 
+        key: 'cavemax',
+        label: 'Actions',
+				fn: function (value, object) {
+					Session.set("boireID", object._id);
+					return new Handlebars.SafeString('<a href="/macave/' + object._id +'/edit">Edit</a>' +' '+ '<a href="" class="modifyStatus">Archiver</a>'); 			
+    	}
+		}
+		
+		
+				] }
+    }
   
 });
 
 Template.tableauVinTemplate.events ({
   'click .modifyStatus': function(e) {
     e.preventDefault();
-   
-    Meteor.call('modifyS', this._id);
+ 
+    Meteor.call('modifyS', Session.get("boireID"));
+		alert(Session.get("boireID"));
   }
   
   
